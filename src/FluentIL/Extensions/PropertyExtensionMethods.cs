d45c3f4da3bdbf67c7ac876e0dfe24f95c99ddea
P Collyer <p.s.collyer@ntlworld.com>
2019-05-08 08:38:28 +0000

    Property changes

--- src/FluentIL/Extensions/PropertyExtensionMethods.cs
+++ src/FluentIL/Extensions/PropertyExtensionMethods.cs
@@ -107,9 +107,35 @@ namespace FluentIL
         /// <returns>The <see cref="IEmitter"/>.</returns>
         public static IEmitter GetProperty(this IEmitter emitter, string propertyName, ILocal local)
         {
-            MethodInfo getMethod = local.LocalType.GetMethod(string.Format("get_{0}", propertyName));
             return emitter
                 .LdLocS(local)
+                .GetProperty(propertyName, local.LocalType);
+        }
+
+        /// <summary>
+        /// Emits IL to load the contents of a property onto the evaluation stack.
+        /// </summary>
+        /// <typeparam name="TProperty">The property type.</typeparam>
+        /// <param name="emitter">The <see cref="IEmitter"/> to use.</param>
+        /// <param name="propertyName">The property name.</param>
+        /// <returns>The <see cref="IEmitter"/>.</returns>
+        public static IEmitter GetProperty<TProperty>(this IEmitter emitter, string propertyName)
+        {
+            return emitter
+                .GetProperty(propertyName, typeof(TProperty));
+        }
+
+        /// <summary>
+        /// Emits IL to load the contents of a property onto the evaluation stack.
+        /// </summary>
+        /// <param name="emitter">The <see cref="IEmitter"/> to use.</param>
+        /// <param name="propertyName">The property name.</param>
+        /// <param name="propertyType">The property type.</param>
+        /// <returns>The <see cref="IEmitter"/>.</returns>
+        public static IEmitter GetProperty(this IEmitter emitter, string propertyName, Type propertyType)
+        {
+            MethodInfo getMethod = propertyType.GetProperty(propertyName).GetGetMethod();
+            return emitter
                 .CallVirt(getMethod);
         }
 
@@ -122,10 +148,35 @@ namespace FluentIL
         /// <returns>The <see cref="IEmitter"/>.</returns>
         public static IEmitter SetProperty(this IEmitter emitter, string propertyName, ILocal local)
         {
-            MethodInfo setMethod = local.LocalType.GetMethod(string.Format("set_{0}", propertyName));
             return emitter
                 .LdLocS(local)
+                .SetProperty(propertyName, local.LocalType);
+        }
+
+        /// <summary>
+        /// Emits IL to call the set method of a property.
+        /// </summary>
+        /// <param name="emitter">The <see cref="IEmitter"/> to use.</param>
+        /// <param name="propertyName">The property name.</param>
+        /// <param name="propertyType">The property type.</param>
+        /// <returns></returns>
+        public static IEmitter SetProperty(this IEmitter emitter, string propertyName, Type propertyType)
+        {
+            var setMethod = propertyType.GetProperty(propertyName).GetSetMethod();
+            return emitter
                 .CallVirt(setMethod);
+        }
+
+        /// <summary>
+        /// Emits IL to call the set method of a property.
+        /// </summary>
+        /// <typeparam name="TProperty">The property type.</typeparam>
+        /// <param name="emitter">The <see cref="IEmitter"/> to use.</param>
+        /// <param name="propertyName">The property name.</param>
+        /// <returns></returns>
+        public static IEmitter SetProperty<TProperty>(this IEmitter emitter, string propertyName)
+        {
+            return emitter.SetProperty(propertyName, typeof(TProperty));
         }
     }
 }
